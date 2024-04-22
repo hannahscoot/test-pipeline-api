6e2a9c1ef6e6156bc36a5e3dafa0e5037a863cb5
hannahscoot <hscott1280@outlook.com>
2024-04-22 19:14:06 +0100

    # updated to actually have a method which calls the host sql server instance

--- test-pipeline/test-pipeline/Controllers/WeatherForecastController.cs
+++ test-pipeline/test-pipeline/Controllers/WeatherForecastController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 
 namespace test_pipeline.Controllers
 {
@@ -28,6 +29,48 @@ namespace test_pipeline.Controllers
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
             .ToArray();
+        }
+
+        [HttpGet("GetTestData")]
+        public List<DemoResult> GetTestData()
+        {
+            var returnobject = new List<DemoResult>();
+
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+
+            builder.DataSource = "192.168.50.30,1433"; //replace with ip of server
+            builder.UserID = "SA";
+            builder.Password = "Password123!";
+            builder.InitialCatalog = "TestDB";
+            builder.TrustServerCertificate = true;
+
+            using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+            {
+                connection.Open();
+
+                string sql = "SELECT * FROM Demo";
+
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            DemoResult result = new DemoResult();
+                            result.Id = (int)reader["Id"];
+                            result.Name = (string)reader["Name"];
+                            returnobject.Add(result);
+                        }
+                    }
+                }
+            }
+            return returnobject;
+        }
+
+        public class DemoResult
+        {
+            public int Id { get; set; }
+            public string? Name { get; set; }
         }
     }
 }
